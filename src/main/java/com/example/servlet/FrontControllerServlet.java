f85c6fec18e3fc60c473f97389f30432dad6709a
randianina07 <randianina07@gmail.com>
2026-06-21 23:55:13 +0300

    sprint1

--- src/main/java/com/example/servlet/FrontControllerServlet.java
+++ src/main/java/com/example/servlet/FrontControllerServlet.java
@@ -2,6 +2,10 @@ package com.example.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
+import com.example.annotation.AnnotationController;
+import com.example.utils.Utilitaire;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,6 +13,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontControllerServlet extends HttpServlet{
+    private List<Class<?>> annotatedClasses;
+    @Override
+    public void init() throws ServletException{
+        super.init();
+
+        String packageName = this.getInitParameter("packageTest");
+
+        this.annotatedClasses = Utilitaire.getClassesAnnotated(packageName, AnnotationController.class);
+    }
     
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
@@ -24,6 +37,14 @@ public class FrontControllerServlet extends HttpServlet{
         resp.setContentType("text/plain");
         String url = req.getRequestURL().toString();
         PrintWriter out = resp.getWriter();
-        out.println(url);       
+        out.println(url);   
+        
+        out.println("URL demandée : " + url);       
+        out.println("-------------------------------------");
+        out.println("Contrôleurs détectés par le Framework :");
+
+        for(Class<?> cls: this.annotatedClasses){
+            out.println(cls.getName());
+        }
     }
 }
